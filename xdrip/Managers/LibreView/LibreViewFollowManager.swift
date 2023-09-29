2c139e54d87d90b1072227c6d8518ce15c5a840c
Johan Degraeve <johan.degraeve@gmail.com>
2023-09-29 23:03:01 +0200

    temp commit

--- xdrip/Managers/LibreView/LibreViewFollowManager.swift
+++ xdrip/Managers/LibreView/LibreViewFollowManager.swift
@@ -1,9 +1,51 @@
 //
-//  LibreViewFollowManager.swift
+//  LibreView.swift
 //  xdrip
 //
-//  Created by Johan Degraeve on 29/09/2023.
+//  Created by Johan Degraeve on 11/09/2023.
 //  Copyright © 2023 Johan Degraeve. All rights reserved.
 //
 
 import Foundation
+import os
+import AVFoundation
+
+class LibreView: NSObject {
+    
+    // MARK: - private properties
+    
+    /// to solve problem that sometemes UserDefaults key value changes is triggered twice for just one change
+    private let keyValueObserverTimeKeeper:KeyValueObserverTimeKeeper = KeyValueObserverTimeKeeper()
+    
+    /// for logging
+    private var log = OSLog(subsystem: ConstantsLog.subSystem, category: ConstantsLog.categoryNightScoutFollowManager)
+    
+    /// when to do next download
+    private var nextFollowDownloadTimeStamp:Date
+    
+    /// reference to coredatamanager
+    private var coreDataManager:CoreDataManager
+    
+    /// reference to BgReadingsAccessor
+    private var bgReadingsAccessor:BgReadingsAccessor
+    
+    /// delegate to pass back glucosedata
+    private (set) weak var libreLinkFollowerDelegate:LibreLinkFollowerDelegate?
+
+    /// AVAudioPlayer to use
+    private var audioPlayer:AVAudioPlayer?
+    
+    /// constant for key in ApplicationManager.shared.addClosureToRunWhenAppWillEnterForeground - create playsoundtimer
+    private let applicationManagerKeyResumePlaySoundTimer = "LibreLinkFollowerManager-ResumePlaySoundTimer"
+    
+    /// constant for key in ApplicationManager.shared.addClosureToRunWhenAppDidEnterBackground - invalidate playsoundtimer
+    private let applicationManagerKeySuspendPlaySoundTimer = "LibreLinkFollowerManager-SuspendPlaySoundTimer"
+    
+    /// closure to call when downloadtimer needs to be invalidated, eg when changing from master to follower
+    private var invalidateDownLoadTimerClosure:(() -> Void)?
+    
+    // timer for playsound
+    private var playSoundTimer:RepeatingTimer?
+
+
+}
